3f12001d8788325a0a8652e414a79c9909b1d84d
kaibob222 <43156001+kaibob222@users.noreply.github.com>
2022-02-11 11:10:41 +0900

    migration

---
+++ groupPassport/groupPassport/Classes/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace groupPassport.Classes
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public String FirstName { get; set; }
+        [MaxLength(50)]
+        public String SurName { get; set; }
+        [MaxLength(50)]
+        public String MiddleName { get; set; }
+
+        [MaxLength(20)]
+        public String Login { get; set; }
+        [MaxLength(20)]
+        public String Password { get; set; }
+
+    }
+}
---
+++ groupPassport/groupPassport/Migrations/202202110207456_m1.cs
@@ -0,0 +1,30 @@
+﻿namespace groupPassport.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class m1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(maxLength: 50),
+                        SurName = c.String(maxLength: 50),
+                        MiddleName = c.String(maxLength: 50),
+                        Login = c.String(maxLength: 20),
+                        Password = c.String(maxLength: 20),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.People");
+        }
+    }
+}
