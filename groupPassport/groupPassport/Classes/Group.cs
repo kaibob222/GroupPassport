92db28fa1f2175c9dbc0eff8082f43ab96fc38ee
timofeeva358456 <ayyyna.timofeeva@mail.ru>
2022-02-11 11:16:29 +0900

    Добавили класс Group

---
+++ groupPassport/groupPassport/Classes/Group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace groupPassport.Classes
+{
+    class Group
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string GroupName { get; set; }
+        [Range(2015, 2100)]
+        public int Year { get; set; }
+    }
+}
