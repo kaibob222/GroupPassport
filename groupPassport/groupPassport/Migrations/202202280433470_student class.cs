1ec4aa5ad4bda765531019c91234a6a5860bf78c
Ambros52 <43155893+Ambros52@users.noreply.github.com>
2022-02-28 13:44:32 +0900

    added class Student

---
+++ groupPassport/groupPassport/Migrations/202202280433470_student class.cs
@@ -0,0 +1,24 @@
+﻿namespace groupPassport.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class studentclass : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.People", "GroupId", c => c.Int());
+            AddColumn("dbo.People", "Discriminator", c => c.String(nullable: false, maxLength: 128));
+            CreateIndex("dbo.People", "GroupId");
+            AddForeignKey("dbo.People", "GroupId", "dbo.Groups", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.People", "GroupId", "dbo.Groups");
+            DropIndex("dbo.People", new[] { "GroupId" });
+            DropColumn("dbo.People", "Discriminator");
+            DropColumn("dbo.People", "GroupId");
+        }
+    }
+}
