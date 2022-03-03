15a51488687bf5c183a2c3af102f832872c73697
kaibob222 <43156001+kaibob222@users.noreply.github.com>
2022-03-03 19:11:11 +0900

    class Teacher

---
+++ groupPassport/groupPassport/Classes/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace groupPassport.Classes
+{
+    class Teacher : Person
+    {
+        [MaxLength (20)]
+        public string Position; //Должность
+       
+    }
+}
