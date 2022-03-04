08723ec2deab13d0252aea7418b0100d4b2ec0d1
aino198 <36023401+aino198@users.noreply.github.com>
2022-03-04 10:36:08 +0900

    chages

---
+++ groupPassport/groupPassport/Migrations/202203040123241_teacher created.cs
@@ -0,0 +1,18 @@
+﻿namespace groupPassport.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class teachercreated : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.People", "Position", c => c.String(maxLength: 20));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.People", "Position");
+        }
+    }
+}
